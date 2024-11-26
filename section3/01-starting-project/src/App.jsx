ee3fec83ac2b24733d45329167d2db628f51e1fa
trangbn580 <trangbn580@gmail.com>
2024-11-26 17:22:25 +0700

    Props and making reusable components

--- section3/01-starting-project/src/App.jsx
+++ section3/01-starting-project/src/App.jsx
@@ -1,21 +1,34 @@
-function Header() {
-    return (
-        <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
-            <h1>React Essentials</h1>
-            <p>
-                Fundamental React concepts you will need for almost any app you are
-                going to build!
-            </p>
-        </header>
-    );
-}
+import reactImg from './assets/react-core-concepts.png';
+import {CORE_CONCEPTS} from './data.js';
+import Header from './components/Headers.jsx';
+import CoreConcept from './components/CoreConcept';
+// import componentImage from './assets/components.png';
+// import jsx from './assets/jsx-ui.png';
+// import stateMng from './assets/state-mgmt.png';
+// import props from './assets/config.png';
 
 function App() {
     return (
         <div>
             <Header/>
             <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            {...CORE_CONCEPTS[0]}
+                        />
+                        <CoreConcept
+                            {...CORE_CONCEPTS[1]}
+                        />
+                        <CoreConcept
+                            {...CORE_CONCEPTS[2]}
+                        />
+                        <CoreConcept
+                            {...CORE_CONCEPTS[3]}
+                        />
+                    </ul>
+                </section>
                 <h2>Time to get started!</h2>
             </main>
         </div>
