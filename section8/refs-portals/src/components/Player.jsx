bfdb87cff018868a12002aaebbe6585bbb071bf7
trangbn <trangbn580@gmail.com>
2024-12-28 11:52:30 +0700

    Adding first refs use

--- section8/refs-portals/src/components/Player.jsx
+++ section8/refs-portals/src/components/Player.jsx
@@ -1,10 +1,20 @@
+import {useState, useRef} from 'react';
+
 export default function Player() {
+
+    const [enteredName, setEnteredName] = useState();
+    const playerName = useRef();
+
+    function handleClick() {
+        setEnteredName(playerName.current.value);
+    }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredName ? enteredName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName} />
+        <button onClick={handleClick}>Set name</button>
       </p>
     </section>
   );
