f57b026b3a55437d5eb1a4da2f5f03abcf10e8c7
trangbn580 <trangbn580@gmail.com>
2025-01-02 10:45:17 +0700

    Show stopped time remaining

--- section8/refs-portals/src/components/ResultModal.jsx
+++ section8/refs-portals/src/components/ResultModal.jsx
@@ -1,6 +1,6 @@
 import {forwardRef, useImperativeHandle, useRef} from 'react';
 
-const ResultModal = forwardRef( function ResultModal({ result, targetTime}, ref) {
+const ResultModal = forwardRef( function ResultModal({ targetTime, timeRemaining, onReset}, ref) {
 
     const dialog = useRef();
 
@@ -12,12 +12,15 @@ const ResultModal = forwardRef( function ResultModal({ result, targetTime}, ref)
         };
     });
 
+    const userLost = timeRemaining <= 0;
+    const formattedRemainingTime = (timeRemaining / 1000).toFixed(2);
+
     return (
         <dialog ref = {dialog} className="result-modal">
-            <h2>You {result}</h2>
+            {userLost && <h2>You lost</h2>}
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
-            <p>You stop the timer with <strong>X seconds left.</strong></p>
-            <form method="dialog">
+            <p>You stop the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
