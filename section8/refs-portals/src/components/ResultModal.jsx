70a4627d949cbb29ad988cb58d4c50443a9bd2e2
trangbn580 <trangbn580@gmail.com>
2025-01-02 13:07:35 +0700

    Correct calculate score

--- section8/refs-portals/src/components/ResultModal.jsx
+++ section8/refs-portals/src/components/ResultModal.jsx
@@ -14,10 +14,12 @@ const ResultModal = forwardRef( function ResultModal({ targetTime, timeRemaining
 
     const userLost = timeRemaining <= 0;
     const formattedRemainingTime = (timeRemaining / 1000).toFixed(2);
+    const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
 
     return (
-        <dialog ref = {dialog} className="result-modal">
+        <dialog ref = {dialog} className="result-modal" onClose={onReset}>
             {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your score: {score}</h2>}
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You stop the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
             <form method="dialog" onSubmit={onReset}>
