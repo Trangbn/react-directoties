055477da60f99f744a5365a5c9aab49eeed99ca8
trangbn580 <trangbn580@gmail.com>
2025-01-02 15:29:14 +0700

    Using createPortal to have dialog in suitable DOM place

--- section8/refs-portals/src/components/ResultModal.jsx
+++ section8/refs-portals/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import {forwardRef, useImperativeHandle, useRef} from 'react';
+import {createPortal} from 'react-dom';
 
 const ResultModal = forwardRef( function ResultModal({ targetTime, timeRemaining, onReset}, ref) {
 
@@ -16,7 +17,7 @@ const ResultModal = forwardRef( function ResultModal({ targetTime, timeRemaining
     const formattedRemainingTime = (timeRemaining / 1000).toFixed(2);
     const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
 
-    return (
+    return createPortal(
         <dialog ref = {dialog} className="result-modal" onClose={onReset}>
             {userLost && <h2>You lost</h2>}
             {!userLost && <h2>Your score: {score}</h2>}
@@ -25,7 +26,8 @@ const ResultModal = forwardRef( function ResultModal({ targetTime, timeRemaining
             <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById("modal")
     );
 });
 
