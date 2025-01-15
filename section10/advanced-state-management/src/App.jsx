2b0c3614db9fcc84a84f24c101dc525f846a4e32
trangbn <trangbn580@gmail.com>
2025-01-15 19:23:56 +0700

    Outsourcing context and state (cont)

--- section10/advanced-state-management/src/App.jsx
+++ section10/advanced-state-management/src/App.jsx
@@ -3,22 +3,22 @@ import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
-import {CartContext} from "./store/shopping-cart-context.jsx";
+import CartContextProvider, {CartContext} from "./store/shopping-cart-context.jsx";
 import Product from "./components/Product.jsx";
 
 function App() {
 
   return (
-    <CartContext.Provider value={ctxValue}>
+    <CartContextProvider>
       <Header/>
-      <Shop onAddItemToCart={handleAddItemToCart}>
+      <Shop>
         {DUMMY_PRODUCTS.map((product) => (
             <li key={product.id}>
               <Product {...product} />
             </li>
         ))}
       </Shop>
-    </CartContext.Provider>
+    </CartContextProvider>
   );
 }
 
