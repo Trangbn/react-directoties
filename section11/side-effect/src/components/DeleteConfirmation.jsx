c53959eabfe319823e387b9af5ea6efe7374e925
trangbn580 <trangbn580@gmail.com>
2025-02-04 16:29:25 +0700

    Adding auto dismiss popup

--- section11/side-effect/src/components/DeleteConfirmation.jsx
+++ section11/side-effect/src/components/DeleteConfirmation.jsx
@@ -1,5 +1,21 @@
+import {useEffect} from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  return (
+
+    console.log("Timer set")
+    useEffect(() => {
+        const timer = setTimeout(()=> {
+            onConfirm();
+        }, 3000);
+
+        return ()=>{
+            console.log("Timer clearing..")
+            clearTimeout(timer);
+        }
+    }, [onConfirm]);
+
+
+    return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
       <p>Do you really want to remove this place?</p>
