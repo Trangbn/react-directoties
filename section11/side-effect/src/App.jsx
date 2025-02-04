8977f72fb9d5b4e92f8ec9adce43d45d1b211675
trangbn580 <trangbn580@gmail.com>
2025-02-04 16:42:19 +0700

    Adding useCallback for extra safety to avoid infinite loop caused by object dependencies

--- section11/side-effect/src/App.jsx
+++ section11/side-effect/src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from 'react';
+import {useCallback, useEffect, useRef, useState} from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -54,17 +54,18 @@ function App() {
 
   }
 
-  function handleRemovePlace() {
-    setPickedPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
-    );
-    setModalIsOpen(false);
+  const handleRemovePlace = useCallback(
+      function handleRemovePlace() {
+        setPickedPlaces((prevPickedPlaces) =>
+            prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+        );
+        setModalIsOpen(false);
 
-    const storeIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
-    localStorage.setItem('selectedPlaces', JSON.stringify(storeIds.filter((id)=>
-      id !== selectedPlace.current
-    )));
-  }
+        const storeIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+        localStorage.setItem('selectedPlaces', JSON.stringify(storeIds.filter((id) =>
+            id !== selectedPlace.current
+        )));
+      }, []);
 
   return (
     <>
