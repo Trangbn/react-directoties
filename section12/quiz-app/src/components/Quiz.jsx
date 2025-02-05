44fd1221f04e7a61d06f570649212d795049782a
trangbn580 <trangbn580@gmail.com>
2025-02-05 14:35:32 +0700

    Shuffle the answers

--- section12/quiz-app/src/components/Quiz.jsx
+++ section12/quiz-app/src/components/Quiz.jsx
@@ -1,11 +1,13 @@
 import {useState} from "react";
 import QUESTIONS from '../question.js';
+import quizCompleted from '../assets/quiz-complete.png';
 
 export default function Quiz() {
 
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(answer) {
         setUserAnswers((prevAnswer) => {
@@ -13,14 +15,26 @@ export default function Quiz() {
         });
     }
 
+    if (quizIsComplete) {
+        return (
+            <div id="summary">
+                <img src={quizCompleted} alt="quiz completed"/>
+                <h2>QUIZ is completed</h2>
+            </div>
+        );
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id="quiz">
             <div id="questions">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {
-                        QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-                                <li key={answer.id} className="answer">
+                        shuffledAnswers.map((answer) => (
+                                <li key={answer} className="answer">
                                     <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                                 </li>
                             )
