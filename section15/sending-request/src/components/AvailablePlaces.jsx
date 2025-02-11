4c52e0605f591498a667b5b01116241d5e68297f
trangbn580 <trangbn580@gmail.com>
2025-02-11 17:28:57 +0700

    Start fetching data from backend

--- section15/sending-request/src/components/AvailablePlaces.jsx
+++ section15/sending-request/src/components/AvailablePlaces.jsx
@@ -1,10 +1,16 @@
 import Places from './Places.jsx';
+import {useState} from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  const response = fetch('http://3000/localhost/places').then(response => response.json());
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
