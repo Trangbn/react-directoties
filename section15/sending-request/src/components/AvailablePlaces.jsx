ff4417f94103a1e11231e9ca2028b427500fffdd
trangbn <trangbn580@gmail.com>
2025-02-15 01:37:16 +0700

    Extracting code and improve code structure

--- section15/sending-request/src/components/AvailablePlaces.jsx
+++ section15/sending-request/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import Places from './Places.jsx';
 import {useEffect, useState} from "react";
 import ErrorPage from './Error.jsx';
 import {sortPlacesByDistance} from "../loc.js";
+import {fetchAvailablePlaces} from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -11,20 +12,17 @@ export default function AvailablePlaces({ onSelectPlace }) {
   useEffect(() => {
     async function fetchPlaces() {
 
-      try {
-        setIsFetching(true);
-        const response = await fetch('http://localhost:3000/places');
-        const resData = await response.json();
+      setIsFetching(true);
 
-        if (!response.ok) {
-          throw new Error("Failed to fetch places");
-        }
+      try {
+
+        const places = await fetchAvailablePlaces();
+
         navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(resData.places, position.latitude, position.longitude);
+          const sortedPlaces = sortPlacesByDistance(places, position.latitude, position.longitude);
           setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
         });
-        setAvailablePlaces(resData.places);
-        setIsFetching(false);
       } catch (error) {
         setError({message: error.message || 'Could not fetch places, try again later... '});
         setIsFetching(false);
