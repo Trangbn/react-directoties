d42cca7ec808e45bbab57c14fcdcc3889df3d49e
trangbn580 <trangbn580@gmail.com>
2025-03-03 16:29:34 +0700

    Handle form submission for form opinion

--- section18/backend-serve-form-action/src/components/NewOpinion.jsx
+++ section18/backend-serve-form-action/src/components/NewOpinion.jsx
@@ -1,23 +1,62 @@
+import {useActionState} from 'react';
+
+function createNewOpinion(prevFormState,formData) {
+  const userName = formData.get('userName');
+  const title = formData.get('title');
+  const body = formData.get('body');
+
+  const errors = [];
+
+  if (!userName) {
+    errors.push('Opinion name is required');
+  }
+
+  if (!title) {
+    errors.push('Opinion title is required');
+  }
+
+  if (!body) {
+    errors.push('Opinion is required');
+  }
+
+  if (errors.length > 0) {
+    return {
+      errors, enteredValues: {
+        userName,
+        title,
+        body
+      }
+    }
+  }
+  return {errors: null}
+}
+
 export function NewOpinion() {
+
+  const [formState,formAction, pending] = useActionState(createNewOpinion, {errors: null});
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form>
+      <form action={formAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
-            <input type="text" id="userName" name="userName" />
+            <input type="text" id="userName" name="userName" defaultValue={formState.enteredValues?.userName} />
           </p>
 
           <p className="control">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" />
+            <input type="text" id="title" name="title" defaultValue={formState.enteredValues?.title} />
           </p>
         </div>
         <p className="control">
           <label htmlFor="body">Your Opinion</label>
-          <textarea id="body" name="body" rows={5}></textarea>
+          <textarea id="body" name="body" rows={5} defaultValue={formState.enteredValues?.body}></textarea>
         </p>
+
+        {formState.errors && <ul className="error">
+          {formState.errors.map(error => <li key={error}>{error}</li>)}
+        </ul>}
 
         <p className="actions">
           <button type="submit">Submit</button>
