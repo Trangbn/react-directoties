a82b28a5d71d63938dbf6fea6dd6d3aea7df64bf
trangbn580 <trangbn580@gmail.com>
2025-03-04 16:36:10 +0700

    Adding css for meal items

---
+++ section19/food-store-practice/src/components/Meals.jsx
@@ -0,0 +1,67 @@
+export function Meals() {
+    return (
+        <div id="meals">
+            <div className="meal-item">
+                <article>
+                    <img src="" alt=""/>
+                    <h3>First meal</h3>
+                    <div className="meal-item-price">15$</div>
+                    <p className="meal-item-description">Noodles are wheat flour and water dough, served in soups or
+                        stir-fries, and are a staple in many cuisines.</p>
+                    <button className="button meal-item-actions">Add to cart</button>
+                </article>
+            </div>
+            <div className="meal-item">
+                <article>
+                    <img src="" alt=""/>
+                    <h3>First meal</h3>
+                    <div className="meal-item-price">15$</div>
+                    <p className="meal-item-description">Noodles are wheat flour and water dough, served in soups or
+                        stir-fries, and are a staple in many cuisines.</p>
+                    <button className="button meal-item-actions">Add to cart</button>
+                </article>
+            </div>
+            <div className="meal-item">
+                <article>
+                    <img src="" alt=""/>
+                    <h3>First meal</h3>
+                    <div className="meal-item-price">15$</div>
+                    <p className="meal-item-description">Noodles are wheat flour and water dough, served in soups or
+                        stir-fries, and are a staple in many cuisines.</p>
+                    <button className="button meal-item-actions">Add to cart</button>
+                </article>
+            </div>
+            <div className="meal-item">
+                <article>
+                    <img src="" alt=""/>
+                    <h3>First meal</h3>
+                    <div className="meal-item-price">15$</div>
+                    <p className="meal-item-description">Noodles are wheat flour and water dough, served in soups or
+                        stir-fries, and are a staple in many cuisines.</p>
+                    <button className="button meal-item-actions">Add to cart</button>
+                </article>
+            </div>
+            <div className="meal-item">
+                <article>
+                    <img src="" alt=""/>
+                    <h3>First meal</h3>
+                    <div className="meal-item-price">15$</div>
+                    <p className="meal-item-description">Noodles are wheat flour and water dough, served in soups or
+                        stir-fries, and are a staple in many cuisines.</p>
+                    <button className="button meal-item-actions">Add to cart</button>
+                </article>
+            </div>
+            <div className="meal-item">
+                <article>
+                    <img src="" alt=""/>
+                    <h3>First meal</h3>
+                    <div className="meal-item-price">15$</div>
+                    <p className="meal-item-description">Noodles are wheat flour and water dough, served in soups or
+                        stir-fries, and are a staple in many cuisines.</p>
+                    <button className="button meal-item-actions">Add to cart</button>
+                </article>
+            </div>
+        </div>
+
+    );
+}
