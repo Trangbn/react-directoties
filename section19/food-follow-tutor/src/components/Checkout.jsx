611a263d323050925dacd7c4f876823fd057002a
trangbn <trangbn580@gmail.com>
2025-03-23 02:06:40 +0700

    Migrating to form action

--- section19/food-follow-tutor/src/components/Checkout.jsx
+++ section19/food-follow-tutor/src/components/Checkout.jsx
@@ -37,12 +37,10 @@ export default function Checkout(){
         clearData();
     }
 
-    function handleSubmit(event){
-        event.preventDefault();
-        const fd = new FormData(event.target);
+    async function checkoutAction(fd){
         const customerData = Object.fromEntries(fd.entries());
 
-        sendRequest(JSON.stringify({
+        await sendRequest(JSON.stringify({
             order: {
                 items: cartCtx.items,
                 customer: customerData
@@ -70,7 +68,7 @@ export default function Checkout(){
     }
 
     return <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleFinish}>
-        <form onSubmit={handleSubmit}>
+        <form action={checkoutAction}>
             <h2>Checkout</h2>
             <p>Total Amount: {currencyFormat.format(cartTotal)}</p>
             <Input label="Fullname" id="name" type="text" placeholder="Full name"/>
