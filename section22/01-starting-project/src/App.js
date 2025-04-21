cadbf3ce58434cafcf96048c5d0e14a4d325650d
trangbn580 <trangbn580@gmail.com>
2025-04-21 17:01:20 +0700

    add page and routes

--- section22/01-starting-project/src/App.js
+++ section22/01-starting-project/src/App.js
@@ -1,5 +1,15 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import HomePage from "./pages/Home";
+import Product from "./pages/Product";
+
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage/>},
+  {path:'/products', element: <Product/>}
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}/>;
+
 }
 
 export default App;
---
+++ section22/01-starting-project/src/pages/Product.js
@@ -0,0 +1,5 @@
+function Product() {
+    return <h1>The product page</h1>
+}
+
+export default Product;
